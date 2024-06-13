05d569e705c52adf1887723c99cfab4061ff4ff0
SirLJM <spambigbox@gmail.com>
2024-06-13 20:49:19 +0200

    refactor for clarity

--- main.py
+++ main.py
@@ -1,23 +1,18 @@
+def main():
+    path = "books/frankenstein.txt"
+    book = read_book(path)
+    num_words = count_words(book)
+    print(f"{num_words} words found in book")
+
+
 def read_book(path):
     with open(path) as f:
-        file_contents = f.read()
-    return file_contents
-
-
-def print_book(book):
-    print(book)
+        return f.read()
 
 
 def count_words(book):
     words = book.split()
     return len(words)
-
-
-def main():
-    path = "books/frankenstein.txt"
-    book = read_book(path)
-    print_book(book)
-    print(count_words(book))
 
 
 main()
