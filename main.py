41a73fdaf09415f6784b59232eb15fc370a4ae43
SirLJM <spambigbox@gmail.com>
2024-06-13 20:43:33 +0200

    add count words method

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
-def print_book(book):
-    with open(book) as f:
+def read_book(path):
+    with open(path) as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
+
+
+def print_book(book):
+    print(book)
+
+
+def count_words(book):
+    words = book.split()
+    return len(words)
 
 
 def main():
-    book = "books/frankenstein.txt"
+    path = "books/frankenstein.txt"
+    book = read_book(path)
     print_book(book)
+    print(count_words(book))
 
 
 main()
