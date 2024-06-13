83602fce78ed6e642da684950c45d2e4655a2b80
SirLJM <spambigbox@gmail.com>
2024-06-13 20:33:48 +0200

    add method to print book

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello world")
+def print_book(book):
+    with open(book) as f:
+        file_contents = f.read()
+    print(file_contents)
+
+
+def main():
+    book = "books/frankenstein.txt"
+    print_book(book)
+
+
+main()
