bb8b80c4a9b79964dbaa4e5f0dc6ca9b5f5c0c1f
SirLJM <spambigbox@gmail.com>
2025-09-14 02:28:07 +0200

    Add a custom book scanning option

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import sys
+
 from stats import count_words
 from stats import count_characters
 from stats import sort_counted_characters
 
 
 def main():
-    path = "books/frankenstein.txt"
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        path = sys.argv[1]
     book = read_book(path)
     num_words = count_words(book)
-    num_characters = count_characters(book)
     sorted_chars = sort_counted_characters(count_characters(book))
+
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at: {path}...")
     print("----------- Word Count ----------")
